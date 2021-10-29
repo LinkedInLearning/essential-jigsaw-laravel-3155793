7c5397ea4589c055d5b3fc22cbd4e5154b2cf886
ZK Recording <zk@Zuzanas-MacBook-Pro.local>
2021-10-29 21:16:38 +0100

    Add 01_03 end files

---
+++ my-blog/config.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Support\Str;
+
+return [
+    'baseUrl' => '',
+    'production' => false,
+    'siteName' => 'Blog Starter Template',
+    'siteDescription' => 'Generate an elegant blog with Jigsaw',
+    'siteAuthor' => 'Author Name',
+
+    // collections
+    'collections' => [
+        'posts' => [
+            'author' => 'Author Name', // Default author, if not provided in a post
+            'sort' => '-date',
+            'path' => 'blog/{filename}',
+        ],
+        'categories' => [
+            'path' => '/blog/categories/{filename}',
+            'posts' => function ($page, $allPosts) {
+                return $allPosts->filter(function ($post) use ($page) {
+                    return $post->categories ? in_array($page->getFilename(), $post->categories, true) : false;
+                });
+            },
+        ],
+    ],
+
+    // helpers
+    'getDate' => function ($page) {
+        return Datetime::createFromFormat('U', $page->date);
+    },
+    'getExcerpt' => function ($page, $length = 255) {
+        if ($page->excerpt) {
+            return $page->excerpt;
+        }
+
+        $content = preg_split('/<!-- more -->/m', $page->getContent(), 2);
+        $cleaned = trim(
+            strip_tags(
+                preg_replace(['/<pre>[\w\W]*?<\/pre>/', '/<h\d>[\w\W]*?<\/h\d>/'], '', $content[0]),
+                '<code>'
+            )
+        );
+
+        if (count($content) > 1) {
+            return $cleaned;
+        }
+
+        $truncated = substr($cleaned, 0, $length);
+
+        if (substr_count($truncated, '<code>') > substr_count($truncated, '</code>')) {
+            $truncated .= '</code>';
+        }
+
+        return strlen($cleaned) > $length
+            ? preg_replace('/\s+?(\S+)?$/', '', $truncated) . '...'
+            : $cleaned;
+    },
+    'isActive' => function ($page, $path) {
+        return Str::endsWith(trimPath($page->getPath()), trimPath($path));
+    },
+];
