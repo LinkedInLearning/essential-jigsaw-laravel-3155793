c58cf8d828f4f1ca1a9b31be5240816f8136d512
ZK Recording <zk@Zuzanas-MacBook-Pro.local>
2021-10-30 12:53:16 +0100

    Add 02_03 end files

--- my-blog/source/_layouts/post.blade.php
+++ my-blog/source/_layouts/post.blade.php
@@ -6,7 +6,7 @@
 
 @section('body')
     @if ($page->cover_image)
-        <img src="{{ $page->cover_image }}" alt="{{ $page->title }} cover image" class="mb-2">
+        <img src="{{ $page->cover_image }}" alt="{{ $page->alt }}" class="mb-2">
     @endif
 
     <h1 class="leading-none mb-2">{{ $page->title }}</h1>
---
+++ my-blog/source/_posts/hello-world.md
@@ -0,0 +1,13 @@
+---
+extends: _layouts.post
+section: content
+title: Hello World
+date: 2021-11-01
+description: My very first post
+cover_image: /assets/img/post-cover-image-2.png
+alt: Hello world
+featured: true
+categories: [configuration]
+---
+
+Hello world, how are you?
